752741563aea827c32376bdf3b06b4fa0ea1df93
raamadithyan <chitha.don@gmail.com>
2023-08-19 03:04:44 +0530

    iconscout added imported directly

--- app/(components)/Sidebar.tsx
+++ app/(components)/Sidebar.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { usePathname } from  "next/navigation"
+import { useMemo } from "react";
+ import UilSearch from '@iconscout/react-unicons/icons/uil-search'
+ import UilHome from '@iconscout/react-unicons/icons/uil-estate'
+
 
 interface SidebarProps {
 	children : React.ReactNode;
@@ -5,9 +12,34 @@ interface SidebarProps {
 
 
 const Sidebar:React.FC<SidebarProps>=({children})=> {
+
+	const pathname = usePathname()
+
+	const routes = useMemo(()=>[
+		{
+			icon: UilSearch,
+			label:'Home',
+			active:pathname!=='/search',
+			href:'/'
+		},
+
+		{
+				
+			label:'Search',
+			active:pathname==='/search',
+			href:'/search'
+		}
+
+		],[pathname])
 	return (
 		<div>
 			{children}
+			{routes.map(r=>r.label)}
+      <UilSearch size="20" color="white" />
+      <UilHome size="20" color="white" />
+
+			
+
 		</div>
 	)
 }
